ff57bf587818832f4e8016c99616d061c985c316
Tuguldur Baatarsuren <19b1num0295@stud.num.edu.mn>
2023-11-09 03:28:44 +0000

    cc

--- my_hash.c
+++ my_hash.c
@@ -1,56 +1,56 @@
 #include "my_hash.h"
 
-void ht_init(HashTable *ht, int len)
+void ht_init(HashTable *p_ht, int len)
 {
     int i;
-    ht->h = (List *)malloc(sizeof(List) * HASH_TABLE_SIZE);
+    p_ht->h = (List *)malloc(sizeof(List) * HASH_TABLE_SIZE);
     for (i = 0; i < HASH_TABLE_SIZE; i++)
-        ht->h[i].head = ht->h[i].tail = NULL;
+        p_ht->h[i].head = p_ht->h[i].tail = NULL;
 }
 
-void ht_insert(HashTable *ht, HashNode x)
+void ht_insert(HashTable *p_ht, HashNode x)
 {
-    int h = ht_hash(x.id);
-    Elm *p_elm = l_search(&ht->h[h], x.id);
+    int h = ht_hash(x.key);
+    Elm *p_elm = l_search(&p_ht->h[h], x.key);
     if (p_elm == NULL)
-        l_push_back(&ht->h[h], x);
+        l_push_back(&p_ht->h[h], x);
 }
 
-HashNode *ht_search(HashTable *ht, const char key[])
+HashNode *ht_search(HashTable *p_ht, const char key[])
 {
     int h = ht_hash(key);
-    Elm *p_elm = l_search(&ht->h[h], key);
+    Elm *p_elm = l_search(&p_ht->h[h], key);
     if (p_elm == NULL)
         return NULL;
     else
         return &(p_elm->x);
 }
 
-void ht_del(HashTable *ht, const char key[])
+void ht_del(HashTable *p_ht, const char key[])
 {
     int h = ht_hash(key);
-    Elm *p_elm = l_search(&ht->h[h], key);
+    Elm *p_elm = l_search(&p_ht->h[h], key);
     if (p_elm != NULL)
-        l_erase(&ht->h[h], h);
+        l_erase(&p_ht->h[h], h);
 }
 
-void ht_release(HashTable *ht)
+void ht_release(HashTable *p_ht)
 {
     int i;
     for (i = 0; i < HASH_TABLE_SIZE; i++)
     {
-        while (ht->h[i].head != NULL)
-            l_pop_front(&ht->h[i]);
+        while (p_ht->h[i].head != NULL)
+            l_pop_front(&p_ht->h[i]);
     }
-    free(ht->h);
+    free(p_ht->h);
 }
 
-unsigned long hash(unsigned char *str)
+unsigned long hash(unsigned char *p_ht)
 {
     unsigned long hash = 5381;
     int c;
 
-    while ((c = *str++))
+    while ((c = *p_ht++))
     {
         hash = ((hash << 5) + hash) + c; // hash * 33 + c
     }
@@ -58,7 +58,10 @@ unsigned long hash(unsigned char *str)
     return hash;
 }
 
-void hashnode_print(FILE *fout, const HashNode *p)
+void hashnode_print(FILE *fout, const HashNode *p_ht)
 {
-    fprintf(fout, "%d\n", p->value);
+    if (p_ht == NULL)
+                fprintf(fout, "xooson\n");
+        else
+                fprintf(fout, "%s\n", p_ht->key);
 }
